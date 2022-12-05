e26ca891b64d42f1c497d66fd8f38b03429b4d1e
LucaFaedda <lucakarl91@gmail.com>
2022-12-05 14:52:57 +0100

    completato es 1?

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,18 @@
-alert('ciao')
+
+// inserire problema
+
+let parola_user = prompt("Inserisci parola")
+
+// creaiamo una variabile prima con split per selezionare lettera per lettera.
+const parola_pali = parola_user.split("")
+console.log(parola_pali)
+
+// usiamo la funzione reverse per invertirla
+let parola_pali2 = parola_pali.reverse(parola_pali)
+console.log(parola_pali2)
+
+
+//infine usiamo la funzione join per riunirla
+let parola_finale = parola_pali2.join("")
+
+console.log(parola_finale)
